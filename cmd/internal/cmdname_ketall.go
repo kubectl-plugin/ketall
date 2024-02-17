4fcfa992697660e32e25bdbc305b844810f96801
Koichi Shiraishi <zchee.io@gmail.com>
2024-02-17 21:15:31 +0900

    all: add 'The kubectl-plugin Authors' to copyright license header
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/internal/cmdname_ketall.go
+++ cmd/internal/cmdname_ketall.go
@@ -1,7 +1,9 @@
+//go:build !getall
 // +build !getall
 
 /*
 Copyright 2019 Cornelius Weig
+Copyright 2024 The kubectl-plugin Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- internal/version/version.go
+++ internal/version/version.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2019 Cornelius Weig
+Copyright 2024 The kubectl-plugin Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
